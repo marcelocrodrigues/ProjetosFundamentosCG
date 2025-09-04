537731211cc6b5acf0f801384816db96f9fe45dc
Marcelo Calsing Rodrigues <151094664+marcelocrodrigues@users.noreply.github.com>
2025-09-04 20:55:53 -0300

    Exclusão do 1º Build (erro)

--- build/CMakeFiles/fc-tmp/glm/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for glm")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Users/Marcelo/Desktop/Git_Prof/PG2025-2/build/CMakeFiles/fc-tmp/glm/glm-gitupdate.cmake")
-
-endblock()
